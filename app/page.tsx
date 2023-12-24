e0cc5c289211acb04f240767b0725a9244538796
orukRed <70902268+orukRed@users.noreply.github.com>
2023-12-24 06:30:18 +0000

    github workspaceからのコミット

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
+      <Link href={"/view"}>
+        viewへ移動するよ
+      </Link>
     </>
   )
 }
