6438c5f60c362204073bbd81c364b0ef6fc62418
orukRed <orukRed@gmail.com>
2024-01-17 21:16:41 +0900

    フォーマッタ適用

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,16 @@
-"use client";
-import { Accordion, AccordionItem, Button } from '@nextui-org/react'
-import Image from 'next/image'
-import Link from 'next/link'
+'use client';
+import { Accordion, AccordionItem, Button } from '@nextui-org/react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <Button>
-        Click me!
-      </Button>
-      
-      <Link href={"/view"}>
-        viewへ移動するよ
-      </Link>
-      <Link href={"/user"}>
-        viewへ移動するよ
-      </Link>
-      <Link href={"/register"}>
-        registerへ移動するよ
-      </Link>
-      
+      <Button>Click me!</Button>
+
+      <Link href={'/view'}>viewへ移動するよ</Link>
+      <Link href={'/user'}>viewへ移動するよ</Link>
+      <Link href={'/register'}>registerへ移動するよ</Link>
     </>
-  )
+  );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,35 +1,41 @@
-"use client";
+'use client';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { faImage } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 config.autoAddCss = false;
-import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from '@nextui-org/react';
 import { LegacyRef, MutableRefObject, useRef, useState } from 'react';
 import styles from './Component.module.css';
-import { PutItemCommand, DynamoDBClient, PutItemCommandInput } from "@aws-sdk/client-dynamodb";
-
 
 interface image {
-  user_id: number,
-  name: string,
-  description: string,
-  file_path: string,
-  ip_address: string,
-  created_at: Date,
-  updated_at: Date | undefined,
-  deleted_at: Date | undefined,
-};
+  user_id: number;
+  name: string;
+  description: string;
+  file_path: string;
+  ip_address: string;
+  created_at: Date;
+  updated_at: Date | undefined;
+  deleted_at: Date | undefined;
+}
 
 const SelectImage = ({ hoge }: any) => {
-
   const [previewSrc, setPreviewSrc] = useState<string | null>(null);
   const fileInputRef: any = useRef(undefined);
   const callFileSelector = () => {
     if (fileInputRef && fileInputRef.current) {
       fileInputRef.current.click();
     }
-  }
+  };
   const selectFile = (e: any) => {
     const file = e.target.files[0];
     const reader = new FileReader();
@@ -37,35 +43,51 @@ const SelectImage = ({ hoge }: any) => {
       if (typeof reader.result === 'string') {
         setPreviewSrc(reader.result);
       }
-    }
+    };
     if (file) {
       reader.readAsDataURL(file);
     }
-
   };
 
   if (previewSrc) {
     return (
       <>
-        <div className={`${styles.checkerboard} flex justify-center items-center`} onClick={callFileSelector}>
-          <input type="file" accept=".png, .jpg, .jpeg, .gif .webp" style={{ display: "none" }} ref={fileInputRef} onChange={selectFile} />
-          <img src={previewSrc}
+        <div
+          className={`${styles.checkerboard} flex justify-center items-center`}
+          onClick={callFileSelector}
+        >
+          <input
+            type='file'
+            accept='.png, .jpg, .jpeg, .gif .webp'
+            style={{ display: 'none' }}
+            ref={fileInputRef}
+            onChange={selectFile}
+          />
+          <img
+            src={previewSrc}
             sizes='10vw'
             width={1}
             height={1}
             style={{
               width: '550px',
               height: '550px',
-              objectFit: 'cover'
-            }} />
-        </div >
+              objectFit: 'cover',
+            }}
+          />
+        </div>
       </>
-    )
+    );
   } else {
     return (
       <>
         <div className={styles.checkerboard} onClick={callFileSelector}>
-          <input type="file" accept=".png, .jpg, .jpeg, .gif .webp" style={{ display: "none" }} ref={fileInputRef} onChange={selectFile} />
+          <input
+            type='file'
+            accept='.png, .jpg, .jpeg, .gif .webp'
+            style={{ display: 'none' }}
+            ref={fileInputRef}
+            onChange={selectFile}
+          />
           <div className='text-xl text-white bg-gray-900 shadow-lg font-bold'>
             ファイルを選択してください
           </div>
@@ -74,10 +96,9 @@ const SelectImage = ({ hoge }: any) => {
           </div>
         </div>
       </>
-    )
+    );
   }
-}
-
+};
 
 //TODO:引数でmodalの表示を切り替える用にするとよさそう
 export default function Page({ isOpening }: { isOpening: boolean }) {
@@ -95,13 +116,12 @@ export default function Page({ isOpening }: { isOpening: boolean }) {
     created_at: new Date(),
     updated_at: undefined,
     deleted_at: undefined,
-  })
+  });
 
   const testFunction: any = () => {
     console.log('Button was clicked!');
-    setCount(cnt => cnt + 1);
+    setCount((cnt) => cnt + 1);
   };
-
 
   const imageData: image = {
     user_id: 0,
@@ -112,72 +132,59 @@ export default function Page({ isOpening }: { isOpening: boolean }) {
     created_at: new Date(),
     updated_at: undefined,
     deleted_at: undefined,
-  }
-  const client = new DynamoDBClient({
-    region: "ap-northeast-1",
-    credentials: {
-      accessKeyId: "hoge",
-      secretAccessKey: "foo",
-    }
-  });
-  const data: PutItemCommandInput = {
-    TableName: 'dynamodb-test',
-    Item: {
-      'user_id': { N: '0' }, // Number type
-      'name': { S: 'test' }, // String type
-      'description': { S: 'test' }, // String type
-      'file_path': { S: 'test' }, // String type
-      'created_at': { S: new Date().toISOString() }, // String type  
-    }
-  }
+  };
   const registerImage = async () => {
     console.log('registerImage');
-    const command = new PutItemCommand(data);
     try {
-      const result = await client.send(command);
-      console.log(result);
+      console.log('send data');
     } catch (error) {
       console.error(error);
     }
-  }
-
+  };
 
   return (
     <>
       {/* <Modal size="4xl" isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false}> */}
-      <Modal size="4xl" isOpen={true} onOpenChange={onOpenChange} isDismissable={false}>
+      <Modal
+        size='4xl'
+        isOpen={true}
+        onOpenChange={onOpenChange}
+        isDismissable={false}
+      >
         <ModalContent>
-          <ModalHeader className="flex flex-col gap-1">image throw</ModalHeader>
+          <ModalHeader className='flex flex-col gap-1'>image throw</ModalHeader>
           <ModalBody>
             <SelectImage />
-            <Input label="name" placeholder="image title" />
-            <Input label="description" placeholder="image description" />
+            <Input label='name' placeholder='image title' />
+            <Input label='description' placeholder='image description' />
           </ModalBody>
           <ModalFooter>
-            <Button color="danger" variant="flat" onPress={onClose}>
+            <Button color='danger' variant='flat' onPress={onClose}>
               Cancel
             </Button>
-            <Button color="primary"
+            <Button
+              color='primary'
               // onClick={() => { testFunction(); registerImage(); onClose(); }}
-              onClick={() => { testFunction(); registerImage(); }}
+              onClick={() => {
+                testFunction();
+                registerImage();
+              }}
             >
               Submit
             </Button>
 
             <div>{count}</div>
             <div>
-              <button onClick={() => setCount(cnt => cnt + 1)}>
+              <button onClick={() => setCount((cnt) => cnt + 1)}>
                 increment
               </button>
-              <Button onClick={() => setCount(cnt => cnt - 1)}>
+              <Button onClick={() => setCount((cnt) => cnt - 1)}>
                 decrement
               </Button>
             </div>
           </ModalFooter>
         </ModalContent>
-      </Modal >
+      </Modal>
     </>
-  )
-
-
+  );
 }
