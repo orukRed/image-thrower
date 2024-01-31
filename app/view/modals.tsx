73f5211d42c9f2ef4128bde731eeb5f7e4f6c6da
orukRed <orukRed@gmail.com>
2024-01-31 12:33:38 +0900

    ページネーション処理の追加

--- app/view/modals.tsx
+++ app/view/modals.tsx
@@ -11,7 +11,7 @@ export default function Modals() {
     <>
       <ImageRegisterModal isOpen={isOpen} onOpenChange={onOpenChange} onClose={onClose} />
       <div>
-        <Button radius='full' color='primary' isIconOnly aria-label='register' onPress={onOpen} className='fixed right-0 bottom-0 m-4 w-20 h-20 md:w-24 md:h-24'>
+        <Button radius='full' color='primary' isIconOnly aria-label='register' onPress={onOpen} className='fixed right-0 bottom-0 m-4 w-16 h-16 md:w-24 md:h-24'>
           <FontAwesomeIcon icon={faPlus} className='text-6xl md:text-7xl' />
         </Button>
       </div>
