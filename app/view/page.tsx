af5b5ba78644a0f7b9612958935d3905f7da2a77
orukRed <orukRed@gmail.com>
2024-02-10 13:02:17 +0900

    ページネーション修正

--- app/view/page.tsx
+++ app/view/page.tsx
@@ -3,25 +3,30 @@ import { firebaseApp, initFirebase } from '@/components/firebase/client';
 import * as firestore from 'firebase/firestore';
 import * as storage from 'firebase/storage';
 import ShowImages from './show-images';
-import { use, useEffect, useState } from 'react';
-import ImageRegisterModal from '@/components/register-image-modal/image-register-modal';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, image, useDisclosure } from '@nextui-org/react';
-import openModal from './modals';
+import { Dispatch, SetStateAction, use, useEffect, useState } from 'react';
 import Modals from './modals';
 import * as fireAuth from 'firebase/auth';
-import { redirect, useSearchParams } from 'next/navigation';
 import PaginationPage from './pagination';
+import { Spacer } from '@nextui-org/react';
 
 export default function View() {
-  const fetchDataFromFirestore = () => {
+  const fetchDataFromFirestore = (lastImage: firestore.DocumentData | null, setLastImage: Dispatch<SetStateAction<firestore.DocumentData | null>>, type: string, firstImage: any, setFirstImage: any) => {
     const db = firestore.getFirestore(firebaseApp);
     const imageCollection = firestore.collection(db, 'images');
-    const query = firestore.query(imageCollection, firestore.where('isPrivate', '==', false), firestore.orderBy('createdAt', 'desc'), firestore.limit(limit));
-
+    let query: any;
+    if (lastImage !== null && firstImage !== null) {
+      if (type === 'start') {
+        query = firestore.query(imageCollection, firestore.where('isPrivate', '==', false), firestore.orderBy('createdAt', 'desc'), firestore.limit(limit), firestore.startAfter(lastImage));
+      } else if (type === 'end') {
+        query = firestore.query(imageCollection, firestore.where('isPrivate', '==', false), firestore.orderBy('createdAt', 'desc'), firestore.limitToLast(limit), firestore.endBefore(firstImage));
+      }
+    } else {
+      query = firestore.query(imageCollection, firestore.where('isPrivate', '==', false), firestore.orderBy('createdAt', 'desc'), firestore.limit(limit));
+    }
     return firestore.getDocs(query).then((imageSnapshot) => {
       const imageList = imageSnapshot.docs.map((doc) => doc.data());
+      setFirstImage(imageSnapshot.docs[0] as firestore.DocumentData | null);
+      setLastImage(imageSnapshot.docs[imageSnapshot.docs.length - 1] as firestore.DocumentData | null);
       return imageList;
     });
   };
@@ -45,17 +50,29 @@ export default function View() {
   };
 
   initFirebase();
-  const limit = 20; //1ページに表示する画像枚数
+  const limit = 10; //1ページに表示する画像枚数
   const [imageList, setImageList] = useState<firestore.DocumentData[]>([]);
   const [imageCount, setImageCount] = useState<number>(0);
-  const searchParams = useSearchParams();
-  const currentPage: number = parseInt(searchParams.get('page') || '1');
+  const [firstImage, setFirstImage] = useState<firestore.DocumentData | null>(null);
+  const [lastImage, setLastImage] = useState<firestore.DocumentData | null>(null);
+  // const currentPage: number = parseInt(searchParams.get('page') || '1');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [previousPage, setPreviousPage] = useState<number>(currentPage);
+  const [type, setType] = useState<string>('start');
   const auth = fireAuth.getAuth();
 
   useEffect(() => {
+    if (previousPage < currentPage) {
+      setType('start');
+    } else if (currentPage < previousPage) {
+      setType('end');
+    }
+    setPreviousPage(currentPage);
+  }, [currentPage]);
+  useEffect(() => {
     const unsubscribe = fireAuth.onAuthStateChanged(auth, (user: any) => {
       if (user) {
-        fetchDataFromFirestore().then((imageList) => {
+        fetchDataFromFirestore(lastImage, setLastImage, type, firstImage, setFirstImage).then((imageList: any) => {
           fetchImagesFromStorage(imageList).then((updatedImageList) => {
             setImageList(updatedImageList);
           });
@@ -66,7 +83,7 @@ export default function View() {
       }
     });
     return () => unsubscribe();
-  }, []);
+  }, [previousPage]);
   if (imageList.length === 0) {
     {
     }
@@ -76,9 +93,11 @@ export default function View() {
   return (
     <>
       <ShowImages imageList={imageList} />
+
       <div className='flex justify-center'>
-        <PaginationPage imageCount={imageCount} imageLimit={limit} currentPage={currentPage} />
+        <PaginationPage imageCount={imageCount} imageLimit={limit} currentPage={currentPage} setCurrentPage={setCurrentPage} />
       </div>
+      <Spacer y={32} />
       <Modals />
     </>
   );
